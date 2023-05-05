c0a831853e6a2d233819787213f7eaf18d582025
Darley Peralta <ordeal00s@gmail.com>
2023-05-05 17:37:37 -0400

    darleyP

--- src/NasaPhotos.js
+++ src/NasaPhotos.js
@@ -1,15 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+
+
+const  NasaDiv = styled.div`  
+  margin: 2rem;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  align-items: center;
+ 
+  
+  h3 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  img {
+    width: 60%;
+    height: auto;
+    margin-bottom: 1rem;
+  }
+
+  .expalate {
+    font-size: 1.1rem;
+    color: #666;
+    margin-top: 1rem;
+    line-height:1.5;
+  }
+};
+`
 
 
 const NasaPhoto = (props) => {
     return (
-        <div className="nasa-photo-wrapper">
+        <NasaDiv >
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
             <img src={props.photo.hdurl}/>
             <p className="expalate">{props.photo.explanation}</p>
-        </div>
+        </NasaDiv>
     )
 }
+
+
+
 
 export default NasaPhoto;
